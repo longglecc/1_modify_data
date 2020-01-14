a7754ffe8592ad0dc4e0b502fae8a0a93ad91de1
longgle <gaolongcc@outlook.com>
2020-01-14 09:10:58 +0800

    added MiniBatchKMeans.

--- model/gc_cluster.py
+++ model/gc_cluster.py
@@ -1,5 +1,6 @@
 
 from sklearn.cluster import KMeans
+from sklearn.cluster import MiniBatchKMeans
 from sklearn import metrics
 import matplotlib.pyplot as plt
 from sklearn.metrics import silhouette_score
@@ -24,12 +25,18 @@ class Model_cluster(object):
         :param param:
         :return:
         """
-        km0 = KMeans(n_clusters=2,init='k-means++',n_init=10,max_iter=300,tol=0.0001,precompute_distances='auto',
+        km0 = KMeans(n_clusters=n,init='k-means++',n_init=10,max_iter=300,tol=0.0001,precompute_distances='auto',
                                verbose=0,random_state=None,copy_x=True,n_jobs=1,algorithm='auto'
                                )
 
         km0.fit(self.data)
 
+        # 中心点
+        print(km0.cluster_centers_)
+        # 每个样本所属的簇
+        print(km0.labels_)
+        # 用来评估簇的个数是否合适，距离越小说明簇分的越好，选取临界点的簇个数
+        print(km0.inertia_)
 
         # print("ACC Score (Train): %f" % clf0.score(self.train_x, self.train_y))
         # print("ACC Score (Test): %f" % metrics.accuracy_score(self.test_y, y_pred0))
@@ -55,8 +62,20 @@ class Model_cluster(object):
         # return Scores
 
 
-    def cluster_XGB(self,param='no'):
-        print(param)
+    def cluster_MiniBatchKMeans(self,n=2,param='no'):
+
+        km0 = MiniBatchKMeans(init='k-means++', n_clusters=n, batch_size=43,
+                      n_init=10, max_no_improvement=10, verbose=0)
+
+
+        km0.fit(self.data)
+        # 中心点
+        print(km0.cluster_centers_)
+        # 每个样本所属的簇
+        print(km0.labels_)
+        # 用来评估簇的个数是否合适，距离越小说明簇分的越好，选取临界点的簇个数
+        print(km0.inertia_)
+
 
     def cluster_other(self,param='no'):
         print(param)
