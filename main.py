8552edd9cc144a62b14820a71400dd30e2e2d64d
Longgle <gaolongcc@outlook.com>
2020-02-06 15:48:21 +0800

    add df_format()

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from model.gc_linear import Model_linear
 from plot import  Model_plot
 from log import  Moddel_log
 from model.gc_cluster import Model_cluster
-from data_proces.gc_process import read_data_csv,init_df_feature_muti,init_df_type_code,get_label_count,trans_label_to_b
+from data_proces.gc_process import read_data_csv,init_df_type_code,get_label_count,trans_label_to_b,split_str,init_df_feature_muti
 import sys
 import numpy as np
-import pandas as dp
+import pandas as pd
 
 
 
@@ -58,8 +58,8 @@ def make_lag_no_balance_inc(df_lag_no):
 
     for i in range(1, 10, 1):
         df_shift = df_data.shift(periods=i, axis=1)
-        df_data_buff = dp.concat([df_data_buff, df_shift])
-        df_tex_buff = dp.concat([df_tex_buff, df_tex])
+        df_data_buff = pd.concat([df_data_buff, df_shift])
+        df_tex_buff = pd.concat([df_tex_buff, df_tex])
 
     df_data_buff.dropna(axis=1, how='any', inplace=True)
     print(df_data_buff.head(5))
@@ -69,7 +69,7 @@ def make_lag_no_balance_inc(df_lag_no):
     print(df_data_buff.shape)
     print(df_tex_buff.shape)
 
-    data_balance = dp.concat([df_tex_buff, df_data_buff], axis=1)
+    data_balance = pd.concat([df_tex_buff, df_data_buff], axis=1)
     print(data_balance.head(5))
     print(data_balance.shape)
     label_columns = data_balance.columns.to_list()
@@ -177,31 +177,85 @@ def make_model(df):
         model_plot = Model_plot()
         model_plot.plot_show(y_test, y_pred)
 
+def df_format(path_ori):
 
+    df_ = read_data_csv(path_ori)
+    # print(df_.head(5))
+    AI_ORDER_COL_NUM = -2
+    df_str_data = df_.iloc[:, AI_ORDER_COL_NUM]
+    df_str_tex = df_.iloc[:, :AI_ORDER_COL_NUM]
+    # get feature in orderarray
+    df_data = pd.DataFrame([split_str(x) for x in df_str_data], index=df_str_data.index).astype('int32')
+    # get columns in df_data
+    columns_count = df_data.shape[1]
+    columns_name = []
+    for col in range(columns_count):
+        if col < columns_count - 1:
+            name = 'feature_{}'.format(col + 1)
+        else:
+            name = 'label'
+        columns_name.append(name)
+    # rename columns
+    df_data.columns = columns_name
+    # concat df with str and data
+    df_formated = pd.concat([df_str_tex, df_data], axis=1)
+    columns_labels = df_formated.columns.to_list()
+    print(df_formated.shape)
+    # sort with label value
+    df_sort = df_formated.sort_values(by=columns_labels[-1], ascending=False)
+    # print(df_sort.head(5))
+    # drop duplicates by feature
+    df_del_same = df_sort.drop_duplicates(subset=columns_labels[-columns_count:-1], keep='first', inplace=False)
+    print(df_del_same.shape)
+    # drop features are all zero
+    df_del_zero = df_del_same.loc[~(df_del_same[columns_labels[-columns_count:-1]] == 0).all(axis=1), :]
+    print(df_del_zero.shape)
+    # print(df_del_zero.head(5))
+    df_sort_index = df_del_zero.sort_index()
+    # print(df_del_zero.head(5))
+    df_sort_index.reset_index(drop=True, inplace=True)
+    print(df_sort_index.head(5))
+    # print(df_del_zero.shape)
+    df_sort_index.to_csv("./data/df_format_sort.csv")
 
 if __name__ == "__main__":
 
     #重定向日志
     sys.stdout = Moddel_log("./log/run.log", sys.stdout)
-    #file_path = "/Users/longgle/Documents/0_work/0_projects/ai/ai_order/2_data/Uintdist_2020-1-3.csv"
-    #完整数据的路径
-    #file_path = "/Users/longgle/Documents/0_work/0_projects/ai/ai_order/2_data/UnitedDist_AIOrderDuplic.csv"
+    # orign data format
+    # path_ori = "../3_data/UnitedDist_AIOrderAnalyze.csv"
+    # df_format(path_ori)
+
+    df_path = "./data/df_format_sort.csv"
+    df_ = read_data_csv(df_path)
+
 
     #NoOther data path
-    lag_no_file_path = "./inter_data/NotOther_data.csv"
-    lag_file_path = "./inter_data/Other_data.csv"
+    # lag_no_file_path = "./inter_data/NotOther_data.csv"
+    # lag_file_path = "./inter_data/Other_data.csv"
     #df = read_data(file_path)
 
-    df_lag_no = read_data_csv(lag_file_path)
+    # df_lag_no = read_data_csv(lag_file_path)
+
+    # print(data_del_zero.shape)
+    # print(df_data.head(5))
+    # label_columns = df_inited.columns.to_list()[n-1]
+    # print(label_columns)
+    # df_label = df_inited.loc[:,[label_columns]]
+    # print(df_label.head(5)
+    # df_label.loc[df_label[df_label.columns.to_list()[0]]>0]=1
+    # print(df_label.head(5))
+    # df_inited[label_columns] = df_label.values
+    # print(df_inited)
 
     #TODO(gaolongcc):(add data analized)
 
-    df_balance = df_lag_no
-    model_linear = Model_linear(df_balance, 0.3)
-    y_test, y_pred = model_linear.exec("LogisticRegression", 'no')
-
-    model_plot = Model_plot()
-    model_plot.plot_show(y_test, y_pred)
+    # df_balance = df_lag_no
+    # model_linear = Model_linear(df_balance, 0.3)
+    # y_test, y_pred = model_linear.exec("LogisticRegression", 'no')
+    #
+    # model_plot = Model_plot()
+    # model_plot.plot_show(y_test, y_pred)
 
 
 
